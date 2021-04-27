b1bda49a6746f7dd54e260505df99d2e1e405f38
Giacomo <giacomo@debugsrl.it>
2021-04-27 09:21:03 +0200

    accelerazioni home create in modo programmatico

--- app/src/main/java/com/example/android/bussolaaccelerometro/HomeFragment.kt
+++ app/src/main/java/com/example/android/bussolaaccelerometro/HomeFragment.kt
@@ -1,19 +1,17 @@
 package com.example.android.bussolaaccelerometro
 
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+import android.app.ActionBar
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.viewModels
+import com.example.android.bussolaaccelerometro.databinding.CardAccelBinding
 
 class HomeFragment : Fragment()
 {
@@ -29,16 +27,53 @@ class HomeFragment : Fragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        val vaccX = view.findViewById<TextView>(R.id.accX)
-        val vaccY = view.findViewById<TextView>(R.id.accY)
-        val vaccZ = view.findViewById<TextView>(R.id.accZ)
+
+        val bindingAccX = CardAccelBinding.inflate(layoutInflater)
+        bindingAccX.accLabel.text = getString(R.string.accel_x)
+        bindingAccX.root.id = View.generateViewId()
+        val bindingAccY = CardAccelBinding.inflate(layoutInflater)
+        bindingAccY.accLabel.text = getString(R.string.accel_y)
+        bindingAccY.root.id = View.generateViewId()
+        val bindingAccZ = CardAccelBinding.inflate(layoutInflater)
+        bindingAccZ.accLabel.text = getString(R.string.accel_z)
+        bindingAccZ.root.id = View.generateViewId()
+
+        val containerAcc = view.findViewById<ConstraintLayout>(R.id.containerAcc)
+
+        containerAcc.addView(bindingAccX.root)
+        containerAcc.addView(bindingAccY.root)
+        containerAcc.addView(bindingAccZ.root)
+
+        val cs = ConstraintSet()
+        cs.connect(bindingAccX.root.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
+        cs.connect(bindingAccX.root.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
+
+        cs.connect(bindingAccY.root.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
+        cs.connect(bindingAccY.root.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
+
+        cs.connect(bindingAccZ.root.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
+        cs.connect(bindingAccZ.root.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
+
+        val chain = arrayOf(bindingAccX.root.id, bindingAccY.root.id, bindingAccZ.root.id)
+        cs.createVerticalChain(ConstraintSet.PARENT_ID, ConstraintSet.TOP,
+            ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM,
+            chain.toIntArray(), null, ConstraintSet.CHAIN_SPREAD)
+
+        cs.applyTo(containerAcc)
+
+
         val vgradiNord = view.findViewById<TextView>(R.id.gradiNord)
         val bussola = view.findViewById<ImageView>(R.id.bussola)
 
-        viewModel.accelX.observe(viewLifecycleOwner, { value -> vaccX.text = "%.2f".format(value)})
-        viewModel.accelY.observe(viewLifecycleOwner, { value -> vaccY.text = "%.2f".format(value)})
-        viewModel.accelZ.observe(viewLifecycleOwner, { value -> vaccZ.text = "%.2f".format(value)})
+        viewModel.accelX.observe(viewLifecycleOwner, { value ->
+            bindingAccX.accValue.text = "%.2f".format(value)
+        })
+        viewModel.accelY.observe(viewLifecycleOwner, { value ->
+            bindingAccY.accValue.text = "%.2f".format(value)
+        })
+        viewModel.accelZ.observe(viewLifecycleOwner, { value ->
+            bindingAccZ.accValue.text = "%.2f".format(value)
+        })
         viewModel.gradiNord.observe(viewLifecycleOwner, { value ->
             vgradiNord.text = "%d °".format(value)
             bussola.rotation = -value.toFloat()
